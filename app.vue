7a8376ee4407bc0924be204c72cdf169aa5ce6e5
Josh Zhong <josh19961201@gmail.com>
2025-01-06 09:32:41 +0800

    update layout & subscribe page

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <template>
-  <NuxtLayout name="default"></NuxtLayout>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 <script setup></script>
---
+++ pages/subscribePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="w-full min-h-screen flex flex-col items-center gap-6 p-8 lg:p-12 text-base"
+  >
+    <h2 class="text-3xl text-center font-bold">
+      新手必學：美股ETF投資精華解密
+    </h2>
+    <div>
+      <p>不只是台股，更大的世界等你發現！透過美股ETF，投資世界級資產一次上手</p>
+      <p>房地產、黃金、科技股 ... 全球資產任你配</p>
+    </div>
+    <div>
+      <p>
+        如果你是... 對未來焦慮的小資族 想擺脫低薪困境的青年
+        想給小孩創造更多財富的父母 秘密 1：如何資產配置美股指數ETF，
+        穩健創造長期報酬？ 秘密 2：如何用 ETF 打造全球投資組合？ 秘密
+        3：用ETF輕鬆投資美國房地產、科技股、黃金、債券 秘密
+        4：如何透過指數化投資，勝過 90% 的專業基金？（實證研究） 秘密
+        5：最強ETF篩選術！用投資工具找出最適合你的ETF
+      </p>
+    </div>
+  </div>
+</template>
+<script setup>
+definePageMeta({
+  layout: false // 停用預設的 layout
+})
+</script>
